62754e51b5d0a05b7b87bab5880b410aa30311db
dibkb <dibas9110@gmail.com>
2023-04-03 19:55:29 +0530

    1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("HEL:O");
+const form = document.querySelector("form");
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  const preview = document.createElement("div");
+  console.log(this.elements[0].value);
+  preview.innerHTML = this.elements[0].value;
+  this.append(preview);
+});
