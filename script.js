ee2da82b7320f6a4d8c8a59d3f2e066d2d6e3e50
dibkb <dibas9110@gmail.com>
2023-04-03 20:00:14 +0530

    popping those alerts

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function (ev) {
   ev.preventDefault();
   const preview = document.createElement("div");
-  console.log(this.elements[0].value);
   preview.innerHTML = this.elements[0].value;
   this.append(preview);
 });
